39e45acc46ac09a4ff5fd50aed2d9895bfaa7c9d
vedha128 <vv908329@gmail.com>
2025-04-01 17:32:19 +0530

    Vedhavalli.N

---
+++ index.js
@@ -0,0 +1,9 @@
+// Smooth scroll animation (optional enhancement)
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener("click", function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute("href")).scrollIntoView({
+            behavior: "smooth"
+        });
+    });
+});
